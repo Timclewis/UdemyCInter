35005dfa11c6a8daeb173e8c7bb1ac7980a23beb
Timclewis <38727434+Timclewis@users.noreply.github.com>
2021-02-12 00:05:38 +0100

    * Stack.cs: * Program.cs: * Stack.csproj:
    * CsInter.sln: Udemy Cinter stack exercise

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(3);
+            stack.Push("meme");
+            stack.Push(true);
+            stack.Push(Math.Pow(2,0.5));
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> _myStack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Object cant be null");
+
+            _myStack.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+            if (_myStack.Count == 0)
+                throw new InvalidOperationException("Stack cant be empty");
+
+            var FinalElement = _myStack[_myStack.Count - 1];
+            _myStack.RemoveAt(_myStack.Count  - 1);
+
+            return FinalElement;
+
+        }
+
+        public void Clear()
+        {
+            _myStack.Clear();
+        }
+    }
+}
